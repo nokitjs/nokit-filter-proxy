68a76b889a2694f01116838e2610e7ecd94905f0
Houfeng <admin@xhou.net>
2016-02-22 21:31:22 +0800

    更新

--- index.js
+++ index.js
@@ -2,9 +2,16 @@ var httpProxy = require("http-proxy");
 
 function ProxyFilter(server) {
     var self = this;
+    var utils = server.require("$./core/utils");
     //读取配置
     self.configs = server.configs.proxy || {};
     self.configs.options = self.configs.options || {};
+    if (utils.isNull(self.configs.options.xfwd)) {
+        self.configs.options.xfwd = true;
+    }
+    if (utils.isNull(self.configs.options.changeOrigin)) {
+        self.configs.options.changeOrigin = true;
+    }
     self.configs.rules = self.configs.rules || {};
     // 新建一个代理 Proxy Server 对象  
     self.proxy = httpProxy.createProxyServer(self.configs.options);
@@ -15,7 +22,13 @@ ProxyFilter.prototype.onRequest = function (context, next) {
     var res = context.res,
         req = context.req;
     for (var expr in self.configs.rules) {
-        if (new RegExp(expr).test(req.url)) {
+        var exprParts = expr.split(' ');
+        var exprText = exprParts[0]
+        var removeText = exprParts[1];
+        if (new RegExp(exprText).test(req.url)) {
+            if (removeText) {
+                req.url = req.url.replace(removeText, "");
+            }
             var target = self.configs.rules[expr];
             return self.proxy.web(req, res, {
                 target: target
